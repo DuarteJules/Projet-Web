f827766a718db746d83b6101ecbfad1e577fc1f4
FERREIRA Luciano <ferreira.luciano@outlook.fr>
2022-04-13 10:41:02 +0200

    wip login signup

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -1,22 +1,20 @@
 const Login =()=>(
-    <>
-        <form action="">
-            <div>
-                <h1>Se Connecter</h1>
-                <p>Remplissez les champs avec vos identifiants</p>
+    <form action="">
+        <div>
+            <h1>Se Connecter</h1>
+            <p>Remplissez les champs avec vos identifiants</p>
 
-                <label htmlFor="username">Nom d'Utilisateur</label>
-                <input type="text" placeholder="entrez votre nom d'utilisateur" name="username" required/>
+            <label htmlFor="username">Nom d'Utilisateur</label>
+            <input type="text" placeholder="entrez votre nom d'utilisateur" name="username" required/>
 
-                <label htmlFor="psw">Mot de passe</label>
-                <input type="text" placeholder="entrez votre mot de passe" name="psw" required/>
+            <label htmlFor="psw">Mot de passe</label>
+            <input type="text" placeholder="entrez votre mot de passe" name="psw" required/>
 
-                <div class="btnContainer">
-                    <button type="submit" class="signupBtn">Se Connecter</button>
-                </div>
+            <div class="btnContainer">
+                <button type="submit" class="signupBtn">Se Connecter</button>
             </div>
-        </form>
-    </>
-    )
+        </div>
+    </form>
+)
     
-    export default Login;
+export default Login;
