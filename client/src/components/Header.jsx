52af234e23269433376de8e7d6974135be48f195
FERREIRA Luciano <ferreira.luciano@outlook.fr>
2022-04-14 17:18:10 +0200

    les putains de changements de filed de merde

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,40 +2,45 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import currentUser from "../functions/user";
 
-// if(loggedin == false){
-    return(
-    <header id="HeaderContainer">
-        <div id={"logo"}>
-                J'ai la <br /> recette
-        </div>
-        <nav id="navbar">
-            <ul id="linkContainer">
-                <p><Link to="/home">Les Recettes</Link></p>
-                <p><Link to="/login">Se Connecter</Link></p>
-                <p><Link to="/signup">S'inscrire</Link></p>
-            </ul>
-        </nav>
-    </header>
-        )
-        // }else{
-//     return(
-//     <header id="HeaderContainer">
-//         <nav id="navbar">
-//             <ul id="linkContainer">
-//                 <li><Link to="/recette">Les Recettes</Link></li>
-//                 <li><Link to="/profil">Mon Profil</Link></li>
-//                 {/* <li><button onClick={logout()}>Se Déconnecter</button></li> */}
-//             </ul>
-//         </nav>
-//     </header>
-// )
-// };
-// function logout(){
-//     loggedin = false;
-// };
+const Header = () => {
 
     const [display, setDisplay] = useState(false);
 
-    }
+    const displayLi = () => {
+        if(currentUser !== null){
+            //display Login et Signup
 
-        export default Header;
+        }else{
+            //display Se Deconnecter
+
+        }
+    }
+    if(currentUser != null){
+        return(
+        <header id="HeaderContainer">
+            <nav id="navbar">
+                <ul id="linkContainer">
+                    <li><Link to="/recette">Recettes</Link></li>
+                    <li><Link to="/homecook">Cuisiner avec mon frigo</Link></li>
+                    <li><a>Se deconnecter</a></li>
+                </ul>
+            </nav>
+        </header>
+        );
+    }else{
+        return(
+            <header id="HeaderContainer">
+            <nav id="navbar">
+                <ul id="linkContainer">
+                    <li><Link to="/recette">Recettes</Link></li>
+                    <li><Link to="/homecook">Cuisiner avec mon frigo</Link></li>
+                    <li><Link to="/login">Se Connecter</Link></li>
+                    <li><Link to="/signup">S'inscrire</Link></li>
+                </ul>
+            </nav>
+        </header>
+        )
+    }
+};
+
+export default Header;
