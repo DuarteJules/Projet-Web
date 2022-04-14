c244911b33bdb8a1334d57e2b4f19f28ebea3491
Jules <jules.duarte@edu.esiee-it.fr>
2022-04-14 16:51:39 +0200

    Auto stash before merge of "main" and "origin/main"

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -1,16 +1,19 @@
 const Login =()=>(
     <>
-        <form action="">
-            <div>
+        <form action="" className="formLogin" >
+            <div className="divLogin">
                 <h1>Se Connecter</h1>
                 <p>Remplissez les champs avec vos identifiants</p>
 
                 <label htmlFor="username">Nom d'Utilisateur</label>
+                <div className="inputLogin">
                 <input type="text" placeholder="entrez votre nom d'utilisateur" name="username" required/>
-
+                </div>
+                
                 <label htmlFor="psw">Mot de passe</label>
+                <div className="inputLogin">
                 <input type="text" placeholder="entrez votre mot de passe" name="psw" required/>
-
+                </div>
                 <div className="btnContainer">
                     <button type="submit" className="signupBtn">Se Connecter</button>
                 </div>
