777bebb20fc15e573c87269635c6c5c0ae0438aa
Philémon Wild <philemon.wild@gmail.com>
2022-04-15 12:14:12 +0200

    luciano caca

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -11,13 +11,6 @@ const getRecipe = async () => {
     // console.log(response);
     return response;
 }
-    // const RECETTES=[
-    // // écrire les recettes au format => { id: 1, name: 'purée', temps: 32, ingredients:'pomme de terre, sel, eau' }
-    // {id:1, name: 'Sardoche', temps: 50, ingredients: 'sel , eau, fun', servings:20 , type:"vegan", image:require("../assets/img/rick.jpg")},
-    // {id:2, name: 'Pipi', temps: 50, ingredients: 'pipi , eau, fun', servings:2 , type:"connard", image:require('../assets/img/rick.jpg')},
-    // {id:3, name: 'Luciano', temps: 50, ingredients: 'portugal , eau, fun', servings:4012 , type:"truc", image:require("../assets/img/rick.jpg")}
-    // ];
-    // // const [RECETTES, getRecipe] 
 
 
 function SearchBar() {
@@ -29,12 +22,6 @@ function SearchBar() {
     // the search result
     const [foundRecipes, setFoundRecipes] = useState(Recipe);
     const loading = useRef(true);
-    // const [Recipe, setRecipe] = useState({});
-
-    // const loading = useRef(true);
-
-    // const [foundRecipes, setFoundRecipes] = useState(Recipe);
-    // const [ingredients, setIngredients] = useState('');
 
 
     useEffect(() => { 
@@ -47,14 +34,6 @@ function SearchBar() {
             })();
         }
         }, []);
-//         const [Recipe, setRecipe] = useState({});
-
-//   // the value of the search field 
-// const [ingredients, setIngredients] = useState('');
-
-//   // the search result
-//     const [foundRecipes, setFoundRecipes] = useState(Recipe);
-    // console.log(Recipe);
 
     const filter = (e) => {
         const keyword = e.target.value;
@@ -90,7 +69,7 @@ function SearchBar() {
                 <Link to={`/Recette/${recette.idRecipe}`} className='RecetteCardContainer' key={recette.idRecipe} >
                 <div >
                     <div className="imageContainer">
-                        <img src={recette.image}/>
+                        <img data-src={{uri:recette.linkImg}}/>
                     </div>
                     <div className="infoRecipeCard">
                         <p>{recette.title}</p>
